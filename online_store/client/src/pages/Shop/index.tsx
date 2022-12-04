f26b8f8ca2e6ab52797a534068ce1de13d25d47b
Oleg-Yg <yudenko_99@bk.ru>
2022-12-04 16:35:56 +0300

    adding a button AddProduct

--- online_store/client/src/pages/Shop/index.tsx
+++ online_store/client/src/pages/Shop/index.tsx
@@ -1,15 +1,32 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import s from "./styles.module.scss";
 import Products from "./Products";
 import { useAppSelector } from "../../hooks/useAppSelector";
 import { useActions } from "../../hooks/useAction";
+import FloatingActionButton from "../../components/FloatingActionButton";
+import useIsAdmin from "../../hooks/useIsAdmin";
+import Tooltip from "../../components/Tooltip";
+import Modal from "../../components/Modal";
+import AddProduct from "./AddProduct";
 
 const Shop: React.FC = () => {
   const { products, error, loading } = useAppSelector((state) => state.product);
   const { fetchProducts } = useActions();
+  const isAdmin = useIsAdmin();
+
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const buttonRef = useRef<HTMLButtonElement | null>(null);
+  // console.log(buttonRef.current);
+  // buttonRef.current;
 
   useEffect(() => {
     fetchProducts();
   }, []);
+
+  const changeModalOpen = () => {
+    setModalOpen(true);
+  };
 
   if (loading) {
     return <h1>Идет загрузка...</h1>;
@@ -19,8 +36,22 @@ const Shop: React.FC = () => {
   }
 
   return (
-    <div style={{ padding: "20px" }}>
+    <div className={s.shop}>
       <Products data={products} />
+      {isAdmin && (
+        <Tooltip title={"Добавить товар"} className={s.tooltipAddButton}>
+          <FloatingActionButton
+            ref={buttonRef}
+            onClick={changeModalOpen}
+            variant={"add"}
+            className={s.addButton}
+          />
+        </Tooltip>
+      )}
+
+      <Modal open={modalOpen} setOpen={setModalOpen} title={"Добавить товар"}>
+        <AddProduct />
+      </Modal>
     </div>
   );
 };
